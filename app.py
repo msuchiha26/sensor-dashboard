58afa3b76ae347bb599d8d775ac25300f01cdfaa
msuchiha26 <mario.silva@correounivalle.edu.co>
2025-07-24 23:40:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import mysql.connector
 import io
 import csv
 from datetime import datetime
+import os
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
